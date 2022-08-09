c376788fad2a352f7c8bc9134287e0a401bd040a
Lucas Mendes dos Santos Mota <lucas.mendes0515@gmail.com>
2022-08-09 20:43:00 -0300

    Feature Lucas - Task: activity of 09/08/22

--- EXDoWhile.java
+++ EXDoWhile.java
@@ -7,21 +7,22 @@ public class EXDoWhile {
 		
 		Scanner leia = new Scanner(System.in);
 		int n,soma=0;
-		//int cont =0;
+		
 		
 		do {
 			
 		System.out.println("\nInfome um numero: ");
 		n = leia.nextInt();
+		soma = n + soma;
 		if(n == 0) {
-			soma = n + n;
 			
+			System.out.println("\nA soma é: " + soma );
 		}
 		
 		
-		}while(n != 0);
-		//soma = n + n;
-		System.out.println("\nA soma é: " + soma );
+		}while(n >= 0);
+		
+		
 		
 		
 		}		
